4889af8576f047fd7d4d19d096f8dd0673cf77f2
jmarizgit <mm@emoriz.com>
2013-01-01 00:48:26 -0800

    hide class

---
+++ xtyle/js/xtyle.dropbox.js
@@ -0,0 +1,16 @@
+/*! xchema | xtyle 2013
+ *  license:  MIT
+ *  author: Mariz Melo
+ *  page: http://xtyle.xchema.com
+ *  github: http://github.com/xchema/xtyle
+ *  desc: Dropbox JavaScript
+ */
+(function($){
+  $.fn.dropbox = function(){ 
+    var $element = "#"+$(this).attr("id"); 
+    var $config = {
+      
+    }
+  }//fn.dropbox
+  
+})(jQuery);
