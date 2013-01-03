3fb42953440490db553baa084d1c2daf3a52146a
jmarizgit <mm@emoriz.com>
2013-01-02 17:50:45 -0800

    package.js added

--- js/xtyle.dropbox.js
+++ js/xtyle.dropbox.js
@@ -8,7 +8,7 @@
 (function($){
   $.fn.dropbox = function(){ 
     var $config = {
-
+      
     }
   }//fn.dropbox
   
---
+++ widgets/menus/dropbox/dropbox.js
@@ -0,0 +1,8 @@
+/*! xchema | xtyle
+ *  created: Jan 2, 2013
+ *  by: Mariz Melo
+ *  desc: Dropbox widget JavaScript file
+ */
+
+// dropdown horizontal
+// dropdown vertical
