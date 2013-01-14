9c663dd78368e0105bf09a5ef226787db32ddb17
Mariz Melo <mm@xchema.com>
2013-01-13 19:28:56 -0800

    can pass value of checkbox on form submit now

--- js/xtyle.js
+++ js/xtyle.js
@@ -32,7 +32,7 @@ $(function(){
                $('span[name='+$(elem).attr('name')+']').removeClass("active");
                 if(!$('span'+elem).hasClass('active')){
                     $('span'+elem).addClass('active');
-                    $('input'+elem).prop('checked', true);
+                    $('input'+elem).attr('checked', true);
                 }//if
             }
         },
@@ -40,14 +40,20 @@ $(function(){
             init : function(){
                 // wrap input type checkbox with spans and hide it
                 $('input[type=checkbox]').each(function(){
-                    $(this).css({'position':'absolute','opacity': .5}).wrap('<span id="'+$(this).attr('id')+'" name="'+$(this).attr('name')+'" class="checkbox" />').wrap('<i class="icon-check" />');
+                    $(this).css({'position':'absolute','opacity': 0}).wrap('<span id="'+$(this).attr('id')+'" name="'+$(this).attr('name')+'" class="checkbox" />').wrap('<i class="icon-check" />');
                 });//each()
                 $('span.checkbox').on('click', function(){
                     x.controller._checkbox.checkBox("#"+$(this).attr("id"));
                 });
             },
             checkBox : function(elem){
-                $('span'+elem).hasClass('active') ? $('span'+elem).removeClass('active') : $('span'+elem).addClass('active');
+                if($('span'+elem).hasClass('active')){
+                    $('span'+elem).removeClass('active');
+                    $('input'+elem).attr('checked', false);
+                }else{
+                    $('span'+elem).addClass('active');
+                    $('input'+elem).attr('checked', true);
+                }//if_else
             }
         }
     }//x.controller
