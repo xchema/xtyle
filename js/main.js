2fcc16bee8bb1cc0b0c28576d7c0eaf5a752ba75
Mariz Melo <mm@xchema.com>
2013-01-27 19:03:48 -0800

    xtyle.js changes on requirejs, fonts coming from cdn now

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 requirejs.config({
   paths: {
+    'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.js',
     'modernizr' : '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min',
     'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
     'widgets' : '../widgets'
