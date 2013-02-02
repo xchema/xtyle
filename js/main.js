afc57a9550865ab64e7deb1b6398404067312613
Mariz Melo <mm@xchema.com>
2013-02-02 02:53:12 -0800

    xtyle.js, border-radius

--- js/main.js
+++ js/main.js
@@ -6,16 +6,6 @@ requirejs.config({
     'widgets' : '../widgets'
   }
 });
-
-// CSS load method
-function loadCss(url) {
-    var link = document.createElement("link");
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = url;
-    document.getElementsByTagName("head")[0].appendChild(link);
-}
-
-require(["modernizr", "jquery", "xtyle"], function($) {
+require(["jquery", "xtyle"], function($) {
 
 });
--- js/xtyle.js
+++ js/xtyle.js
@@ -1,144 +1,145 @@
 /*
  *  xtyle.js MIT Source Code
  *  Copyright (c) 2013 xchema
- *  xtyle source code if a free program
  *  release: 2013-31-01
+ *  dependencies: RequireJS, jQuery
  *  website: http://xtyle.xchema.com
  *  repository: http://github.com/xchema/xtyle
  */
+define( [ 'jquery' ], function ( $ ) {
+( function ( $ ){
+  "use strict";
+  // VARIABLES
+  var self = this, // Reference to current object
+      xtyle = function () {}, // Name-space
+      x = xtyle.prototype;
 
-requirejs.config({
-  paths: {
-    'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.js',
-    'modernizr' : '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min',
-    'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
-    'widgets' : '../widgets'
-  }
-});
+  // CONFIGURATION
+  x.version = {
+    name : "xtyle",
+    version : 0.1,
+    stability : 1 // 1 - Stable, 2 - Unstable, 3 - Experimental
+  };// x.version
 
-define ( [ 'jquery' ], function ( $ ) {
 
-  (function (window, $, undefined) {
-    "use strict"; // faster JavaScript
+  // MODEL
+  x.model = {
+    debug : true,
+    modules : [ 'radio', 'checkbox' ],
+    widgets : {}
+  };//x.model
 
-    var self = this; // Reference to current object
-    var xtyle = function () {}; // Name-space
-    var x = xtyle.prototype;
-
-    x.info = {
-      name : "xtyle",
-      version : 0.1,
-      stability : 1 // 1 - Stable, 2 - Unstable, 3 - Experimental
-    };// x.info
-
-    x.model = {
-      modules : [ 'radio', 'checkbox', 'select' ],
-      widgets : {}
-    };//x.model
-
-    x.controller = {
-      _loadWidgets : function ( widget ) {
-        console.log();
-      },
-      _loadCss : function ( url ) {
-        var link = document.createElement( 'link' );
-        link.type = 'text/css';
-        link.rel = 'stylesheet';
-        link.href = url;
-        document.getElementsByTagName( "head" )[ 0 ].appendChild( link );
-      },
-      _input : {
-        resizeInput : function () {
-          var elem = 'input[ xtyle=full ]';
-          if( $( elem ).length ) {
-            $( elem ).width( $( elem ).parent().width() - ( $( elem ).outerWidth() - $( elem ).width() ) );
-          }
-        },
-        text : {},
-        email : {},
-        number : {},
-        password : {}
-      },
-      _radio : {
-        init : function(){
-          // wrap input type radio with spans and hide it
-          $('input[type=radio]').each(function(){
-            $(this).css({'position':'absolute','opacity': 0}).wrap('<span id="'+$(this).attr('id')+'" name="'+$(this).attr('name')+'" class="radio" />').wrap('<span />');
-            if( $(this).is(':checked') ) $(this).parent().parent().addClass('active');
-          });//each()
-          $('span.radio').on('click', function(){
-            x.controller._radio.radioButton("#"+$(this).attr("id"));
-          });//on()
-        },
-        radioButton : function(elem){
-          $('span[name='+$(elem).attr('name')+']').removeClass("active");
-          if(!$('span'+elem).hasClass('active')){
-            $('span'+elem).addClass('active');
-            $('input'+elem).attr('checked', true);
-          }//if
-        }
-      },
-      _checkbox : {
-        init : function(){
-          // wrap input type checkbox with spans and hide it
-          $('input[type=checkbox]').each(function(){
-            $(this).css({'position':'absolute','opacity': 0}).wrap('<span id="'+$(this).attr('id')+'" name="'+$(this).attr('name')+'" class="checkbox" />').wrap('<i class="icon-check" />');
-            if( $(this).is(':checked') ) $(this).parent().parent().addClass('active');
-          });//each()
-          $('span.checkbox').on('click', function(){
-            x.controller._checkbox.checkBox("#"+$(this).attr("id"));
-          });
-        },
-        checkBox : function(elem){
-          if($('span'+elem).hasClass('active')){
-            $('span'+elem).removeClass('active');
-            $('input'+elem).attr('checked', false);
-          }else{
-            $('span'+elem).addClass('active');
-            $('input'+elem).attr('checked', true);
-          }//if_else
-        }
+  // CONTROLLER
+  x.controller = {
+    _debug : function ( msg ) {
+      if ( x.model.debug ) {
+        console.log ( msg );
       }
-    };//x.controller
-
-    x.view = {
-      events : function(){
-        // Initialize Widgets
-        if($.inArray('fancybox', x.model.widgets) >= 0){
-         if($("a[xtyle=fancybox]").length){
-           $("a[xtyle=fancybox]").fancybox();
-         }
+    },
+    _loadWidget : function ( widget, location ) {
+      require( [ location ], function () {
+        $("."+widget) [ widget ] ();
+      });
+      x.controller._debug( "Widget [ \"" + widget + "\" ] loaded!")
+    },
+    _loadModule : function ( module ) {
+      x.controller['_'+module].init();
+    },
+    _loadCss : function ( url ) {
+      var link = document.createElement( 'link' );
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      link.href = url;
+      document.getElementsByTagName( "head" )[ 0 ].appendChild( link );
+    },
+    _input : {
+      resizeInput : function () {
+        var elem = 'input[ xtyle=full ]';
+        if( $( elem ).length ) {
+          $( elem ).width( $( elem ).parent().width() - ( $( elem ).outerWidth() - $( elem ).width() ) );
         }
-        // Initialize Modules
-        if($.inArray('radio', x.model.modules) >= 0){
-          x.controller._radio.init();
+      },
+      text : {},
+      email : {},
+      number : {},
+      password : {}
+    },
+    _radio : {
+      init : function(){
+        // wrap input type radio with spans and hide it
+        $('input[type=radio]').each(function(){
+          $(this).css({'position':'absolute','opacity': 0}).wrap('<span id="'+$(this).attr('id')+'" name="'+$(this).attr('name')+'" class="radio" />').wrap('<span />');
+          if( $(this).is(':checked') ) $(this).parent().parent().addClass('active');
+        });//each()
+        $('span.radio').on('click', function(){
+          x.controller._radio.radioButton("#"+$(this).attr("id"));
+        });//on()
+      },
+      radioButton : function(elem){
+        $('span[name='+$(elem).attr('name')+']').removeClass("active");
+        if(!$('span'+elem).hasClass('active')){
+          $('span'+elem).addClass('active');
+          $('input'+elem).attr('checked', true);
         }//if
-        if($.inArray('checkbox', x.model.modules) >= 0){
-          x.controller._checkbox.init();
-        }//if
-        // Click on labels with attribute "for"
-        $('label').on('click', function(){
-          var type = $('input#'+$(this).attr('for')).attr('type');
-          switch(type){
-            case 'radio':
-            x.controller._radio.radioButton('#'+$(this).attr('for'));
-            break;
-            case 'checkbox':
-            x.controller._checkbox.checkBox('#'+$(this).attr('for'));
-            break;  
-          }//switch
+      }
+    },
+    _checkbox : {
+      init : function(){
+        // wrap input type checkbox with spans and hide it
+        $('input[type=checkbox]').each(function(){
+          $(this).css({'position':'absolute','opacity': 0}).wrap('<span id="'+$(this).attr('id')+'" name="'+$(this).attr('name')+'" class="checkbox" />').wrap('<i class="icon-check" />');
+          if( $(this).is(':checked') ) $(this).parent().parent().addClass('active');
+        });//each()
+        $('span.checkbox').on('click', function(){
+          x.controller._checkbox.checkBox("#"+$(this).attr("id"));
         });
-        
-        // Input full width
-        /*x.controller._input.resizeInput();
-        $(window).resize(function() {
-            x.controller._input.resizeInput();
-        });*/
-      }()
-    };//x.view 
+      },
+      checkBox : function(elem){
+        if($('span'+elem).hasClass('active')){
+          $('span'+elem).removeClass('active');
+          $('input'+elem).attr('checked', false);
+        }else{
+          $('span'+elem).addClass('active');
+          $('input'+elem).attr('checked', true);
+        }//if_else
+      }
+    }
+  };//x.controller
 
-    window.xtyle = x; // Return global object 
+  // INIT
+  x.init = function(){
 
-  })(window);
+      // VISUAL MODULES
+      // modules is an array
+      $.each( x.model.modules, function(i, module){ //i - index on array
+         x.controller._loadModule ( module );
+      });
+      // CALL WIDGETS
+      // widgets is an object
+      $.each( x.model.widgets, function ( widget, location ) {
+        x.controller._loadWidget ( widget, location );
+      });
 
-});// RequireJS module
+      // Click on labels with attribute "for"
+      $('label').on('click', function(){
+        var type = $('input#'+$(this).attr('for')).attr('type');
+        switch(type){
+          case 'radio':
+          x.controller._radio.radioButton('#'+$(this).attr('for'));
+          break;
+          case 'checkbox':
+          x.controller._checkbox.checkBox('#'+$(this).attr('for'));
+          break;  
+        }//switch
+      });
+
+  }();
+
+  window.xtyle = x; // Return global object
+
+  //Expose xtyle for AMD modules like RequireJS
+  if ( typeof define === "function" && define.amd ) {
+    define( "xtyle", [], function () { return window.xtyle; } );
+  }
+})(jQuery);
+});// define
