e66f302c4009b6e0d2db6e8f072b7040653bac8f
Mariz Melo <mm@xchema.com>
2013-02-02 03:29:11 -0800

    fix path for main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,13 @@ requirejs.config({
     }
   },
   paths: {
-    'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.js',
+    'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text',
     'modernizr' : '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min',
     'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
     'xtyle' : 'js/xtyle',
     'widgets' : '../widgets'
-  }
+  },
+  urlArgs: ""+(new Date()).getTime()  // REMOVE WHEN IN PRODUCTION - AVOID CACHE FOR DEVELOPMENT
 });
 
-require(["xtyle"], function() {} );
+require([ 'xtyle' ], function( xtyle ) {} );
