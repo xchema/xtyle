15d9b9069782a79bd13ae9bfec824aa919f84311
Mariz Melo <mm@xchema.com>
2013-02-02 03:00:59 -0800

    main.js changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 requirejs.config({
+  baseUrl : 'xtyle',
+  shim: {
+    'xtyle' : {
+      deps : ['jquery']
+    }
+  },
   paths: {
     'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text.js',
     'modernizr' : '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min',
     'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
+    'xtyle' : 'js/xtyle',
     'widgets' : '../widgets'
   }
 });
-require(["jquery", "xtyle"], function($) {
 
-});
+require(["xtyle"], function() {} );
