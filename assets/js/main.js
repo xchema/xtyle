5a5cbfbc025a61eb14e7c877e91f9e8619650960
Mariz Melo <mm@xchema.com>
2013-02-03 23:14:58 -0800

    using minified version of xtyle.js now

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ requirejs.config({
   baseUrl: "assets/js",
   paths: {
     'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
-    'xtyle' : 'https://s3-us-west-1.amazonaws.com/xtyle/xtyle'
+    'xtyle' : 'https://s3-us-west-1.amazonaws.com/xtyle/xtyle.min'
   }
 });
 
