b5c37993311626cb4e2401b3238413b3978a23bf
Mariz Melo <mm@xchema.com>
2013-02-06 16:32:45 -0800

    merge master

--- xtyle/widgets/social/addthis/addthis.js
+++ xtyle/widgets/social/addthis/addthis.js
@@ -1,4 +1,7 @@
-define(["jquery", "text!widgets/social/addthis/addthis.html", "require"], function($, addthisHtml, require){
-  console.log("AddThis widget loaded");
-  loadCss(require.toUrl("./addthis.css"));
-});
+(function($){
+  
+  $.fn.addthis = function(){
+    // code here
+  }// fn.addthis
+  
+})(jQuery);  
