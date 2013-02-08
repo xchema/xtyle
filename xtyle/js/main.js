42ec5eeea2bc4683dd1e4dacc298c14a754f6272
Mariz Melo <mm@xchema.com>
2013-02-08 10:03:40 -0800

    removed CDN fonts, removed modernizr, cache enabled

--- xtyle/js/main.js
+++ xtyle/js/main.js
@@ -7,12 +7,10 @@ requirejs.config({
   },
   paths: {
     'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text',
-    'modernizr' : '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min',
     'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
     'xtyle' : 'js/xtyle',
     'widgets' : 'widgets'
   }
-  , urlArgs: ""+(new Date()).getTime()  // REMOVE WHEN IN PRODUCTION - AVOID CACHE FOR DEVELOPMENT
 });
 
 // CALL XTYLE
