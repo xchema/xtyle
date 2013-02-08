af4c4253cc207564d3b31e4675a05470841f07f4
Mariz Melo <mm@xchema.com>
2013-02-08 11:33:27 -0800

    removed $ from variables on slidejs

--- xtyle/widgets/slideshow/slidejs/slidejs.js
+++ xtyle/widgets/slideshow/slidejs/slidejs.js
@@ -8,50 +8,50 @@
 	
 	$.fn.slidejs = function(){
 
-		var $element = "#" + $(this).attr("id")+" .widgets-slideshow-slidejs"; //cache id for the slide div wrapper
-		// console.log($element);
+		var element = "#" + $(this).attr("id")+" .widgets-slideshow-slidejs"; //cache id for the slide div wrapper
+	
 		var play;	//will hold the set-interval event
 		
 		//configuration variable
-		var $config = {
+		var config = {
 			controls : 1,	//show or hide controls
 			interval : "3",	//number of seconds before go to next slide
 			speed : "400",	//speed of transitions
 			slide : 1,	//start from this slide
 			pauseoverimage : 1,	//pause when mouse is over slide images
 			pauseovernavigation : 1,	//pause when mouse is over navigation buttons (after click on them)
-			size : $($element+" .images").children("dd").length-1,	//number of item on the SlideShow
-			width : $($element+" .images").css("width").replace("px","")
+			size : $(element+" .images").children("dd").length-1,	//number of item on the SlideShow
+			width : $(element+" .images").css("width").replace("px","")
 		};
 		
 		//show menu
-		if($config.controls){
-			$($element+" .navigation").css({"display":"block"});
+		if(config.controls){
+			$(element+" .navigation").css({"display":"block"});
 		}
 		
 		//increases size of image wrapper
-		$($element+" .images").css({"width": (($config.size+1)*$config.width+"px")});
-		$($element+" .viewport").css({"width" : $config.width+"px"});
-		$($element+" .images dd").css({"width" : $config.width+"px"});
+		$(element+" .images").css({"width": ((config.size+1)*config.width+"px")});
+		$(element+" .viewport").css({"width" : config.width+"px"});
+		$(element+" .images dd").css({"width" : config.width+"px"});
 		
 		//GO TO SPECIFIC SLIDE
-		var gotoSlide = function($goto){
+		var gotoSlide = function(goto){
 			
-			$config.slide = $goto;
+			config.slide = goto;
 			
-			$($element+" .navigation dd").each(function(){
+			$(element+" .navigation dd").each(function(){
 				$(this).removeClass("active");
 			});
 			
-			$($element+" .navigation dd:nth-child("+($config.slide+1)+")").addClass("active");
+			$(element+" .navigation dd:nth-child("+(config.slide+1)+")").addClass("active");
 			
 			var move;
-			if($config.slide <= $config.size)
-				move = "-"+ $config.slide * $config.width + "px";
+			if(config.slide <= config.size)
+				move = "-"+ config.slide * config.width + "px";
 			else
 				move = 0;
 				
-			$($element+" .images").animate({"margin-left": move }, $config.speed);
+			$(element+" .images").animate({"margin-left": move }, config.speed);
 			
 			
 		};//gotoSlide()		
@@ -60,18 +60,18 @@
 		//CREATES LOOP EVENT
 		var playSlide = function(){
 			 play = self.setInterval(function() {
-			 	if($config.slide > $config.size){
-					$config.slide = 0;}//go to start of slideshow
-	    			gotoSlide($config.slide);
-	    			$config.slide++;
-			}, $config.interval*1000);
+			 	if(config.slide > config.size){
+					config.slide = 0;}//go to start of slideshow
+	    			gotoSlide(config.slide);
+	    			config.slide++;
+			}, config.interval*1000);
 		};
 		
 		//INITIALIZE PLUGIN
 		playSlide();
 		
 		//CONTROLS
-		$($element+" .navigation dd").click(function(event){
+		$(element+" .navigation dd").click(function(event){
 			event.preventDefault();
 			window.clearInterval(play);
 			gotoSlide($(this).index());
@@ -81,7 +81,7 @@
 		});
 		
 		//PAUSE LOOP
-		if($config.pauseoverimage){
+		if(config.pauseoverimage){
 			$(".images").mouseenter(function(event){
 				window.clearInterval(play);
 			}).mouseleave(function(){
