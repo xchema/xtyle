5e479a29c466c01e10ef07340672a84e1ca70864
Mariz Melo <mm@xchema.com>
2013-02-28 13:39:07 -0800

    Update main.js

--- _site/xtyle/js/main.js
+++ _site/xtyle/js/main.js
@@ -9,7 +9,7 @@ requirejs.config({
     'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text',
     'modernizr' : '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min',
     'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
-    'xtyle' : '/xtyle/js/xtyle',
+    'xtyle' : 'xtyle/js/xtyle',
     'widgets' : 'widgets'
   }
   , urlArgs: ""+(new Date()).getTime()  // REMOVE WHEN IN PRODUCTION - AVOID CACHE FOR DEVELOPMENT
