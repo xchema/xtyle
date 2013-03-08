ed5d6590c42883e43e9cc4a3d7c3576b08d762c1
Mariz Melo <mm@xchema.com>
2013-03-08 00:32:46 -0800

    using local files on newlook jekyll website, line-height redefined, download button on homepage

--- newlook/xtyle/js/main.js
+++ newlook/xtyle/js/main.js
@@ -1,9 +1,19 @@
 requirejs.config({
-  baseUrl: "assets/js",
+  baseUrl : '/xtyle',
+  shim: {
+    'xtyle' : {
+      deps : ['jquery']
+    }
+  },
   paths: {
+    'text' : '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text',
+    'modernizr' : '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min',
     'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
-    'xtyle' : 'https://s3-us-west-1.amazonaws.com/xtyle/xtyle.min'
+    'xtyle' : 'js/xtyle',
+    'widgets' : 'widgets'
   }
+  , urlArgs: ""+(new Date()).getTime()  // REMOVE WHEN IN PRODUCTION - AVOID CACHE FOR DEVELOPMENT
 });
 
-require(["jquery", "xtyle"], function($) { });
+// CALL XTYLE
+require( [ 'xtyle' ], function ( xtyle ) {} );
