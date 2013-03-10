a4202ad26a5f82a54b184a8b4f89f6d20b1c0df0
Mariz Melo <mm@xchema.com>
2013-03-10 16:44:38 -0700

    merge newlook

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-  baseUrl: "assets/js",
-  paths: {
-    'jquery' : '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
-    'xtyle' : 'https://s3-us-west-1.amazonaws.com/xtyle/xtyle.min'
-  }
-});
-
-require(["jquery", "xtyle"], function($) { });
